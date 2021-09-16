0eb0f29b335be24185ff8cf80a421c91962e4281
Ben Stacey <benjaminstacey87@gmail.com>
2021-09-16 14:56:59 +1200

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+    public static void main(String[]args){
+        try{
+            int port = Integer.parseInt(args[0]);
+            Socket socket = new Socket(args[1], port);
+            System.err.println("Connected to " + args[1] + ", port: " + port);
+            new ReadWriteThread(socket.getInputStream(), System.out).start();
+            new ReadWriteThread(System.in, socket.getOutputStream()).start();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+            System.err.println("\nProgram usage: java Server <port>");
+        }
+    }
+}
